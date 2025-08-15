432ce6e463a234eab68ba43406c8bd6401dd6164
EnricoSB <enrico.barduco@gmail.com>
2025-08-15 16:43:00 -0300

    fix/ hiding the password

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 url = "https://api.football-data.org/v4/competitions/2013/teams"
-headers = {"X-Auth-Token":"52da30433cfc4394b39d8ec14da17145"}
+headers = {"X-Auth-Token":os.getenv("TOKEN_FOOTBALL")}
 body = {"season":2025}
 teste = requests.get(url=url,headers=headers,params= body).json()
 print(teste)
